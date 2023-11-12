e6055e75992475dd7562f6e4619302c47bd58b79
mdhamimhowladerasif <mdhamimhowladerasif@gmail.com>
2023-11-13 02:33:31 +0600

    first commit

---
+++ chain.js
@@ -0,0 +1,2 @@
+const users = [{id: 1, name:'Abul', job: 'doctor'}];
+console.log(users[0].name);
---
+++ map.js
@@ -0,0 +1,28 @@
+// const number = 123;
+// console.log(length(number));
+// const len  = length(number);
+// console.log(len);
+
+// function min(nums){ 
+//     return Math.min(nums) 
+//   }
+
+// const cube=x=> x*x*x; 
+// console.log(cube(2))
+
+// const [a, b] = [1,2,3,4,45,5]; 
+// console.log(a+b);
+
+// const nums = [1,2,3,4,6,5];
+// let output = nums.filter(n => n%2);
+// console.log(output);
+
+// const friends = ["Moushumi", "Misha", "Manna", "mimi", "mahiya"];
+
+// const friendWithFiveCharacters = friends.find(friend => friend.length === 5);
+
+// if (friendWithFiveCharacters) {
+//   console.log(`The first friend with a name of five characters is: ${friendWithFiveCharacters}`);
+// } else {
+//   console.log("No friend found with a name of five characters.");
+// }
